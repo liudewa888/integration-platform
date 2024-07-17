a80b6fffd1d5665c1f9ac033f3aba36f5c2865dd
guangwei.yu <guangwei.yu@caetar.com>
2024-07-17 19:27:06 +0800

    fix: demo1 单例模式改造

--- public/project/demo1/src/main.js
+++ public/project/demo1/src/main.js
@@ -1,15 +1,28 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import { router } from "./router/index";
-import App from './App.vue'
+import App from "./App.vue";
 
-import './assets/css/base.css'
-import './assets/css/normalize.css'
-import './assets/css/main.css'
+import "./assets/css/base.css";
+import "./assets/css/normalize.css";
+import "./assets/css/main.css";
 
 import { vueErrorHandler } from "./utils/errorHandler";
 
-const app = createApp(App)
-
-app.use(router)
-app.use(vueErrorHandler);
-app.mount('#app')
+if (window.__POWERED_BY_WUJIE__) {
+  let instance;
+  window.__WUJIE_MOUNT = () => {
+    instance = createApp(App);
+    instance.use(router);
+    instance.use(vueErrorHandler);
+    instance.mount("#app");
+  };
+  window.__WUJIE_UNMOUNT = () => {
+    instance.unmount();
+  };
+  window.__WUJIE.mount()
+} else {
+  const app = createApp(App);
+  app.use(router);
+  app.use(vueErrorHandler);
+  app.mount("#app");
+}
